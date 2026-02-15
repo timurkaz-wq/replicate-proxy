d8e3620ad9dba1156c1ab4fd95465be5134b2efa
timurkaz-wq <timurkaz@gmail.com>
2026-02-15 15:10:23 +0500

    Update server.js

--- server.js
+++ server.js
@@ -35,3 +35,14 @@ app.listen(PORT, () => {
   console.log("Server started on port " + PORT);
 });
 
+// Добавьте это в ваш proxy.js на сервере Render
+app.use('/bytedance', createProxyMiddleware({
+    target: 'https://cv-api.bytedance.com', // или другой эндпоинт API ByteDance
+    changeOrigin: true,
+    pathRewrite: { '^/bytedance': '' },
+    onProxyReq: (proxyReq, req, res) => {
+        if (req.headers.authorization) {
+            proxyReq.setHeader('Authorization', req.headers.authorization);
+        }
+    }
+}));
